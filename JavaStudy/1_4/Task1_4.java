c8e25898c38dc6aeb636747f05218e7ff8d55d9b
YugaSeri <awcrowpile0629@outlook.jp>
2023-07-17 17:43:25 +0900

    1_4

--- JavaStudy/1_4/Task1_4.java
+++ JavaStudy/1_4/Task1_4.java
@@ -49,7 +49,7 @@ public class Task1_4 {
         // ③ 「USER_RNAME」の値のみ等しい場合。
        //     定数を使用して「 パスワードに誤りがあります。 」 と出力してください。
 
-       if (name == USER_NAME && pass != USER_PASSWORD){
+       else if (name == USER_NAME && pass != USER_PASSWORD){
         
             System.out.println(CONST_MSG_ERROR_PASS);
        }
@@ -57,7 +57,7 @@ public class Task1_4 {
         // ④ 「USER_PASSWORD」の値のみ等しい場合。
        //定数を使用して「 名前に誤りがあります。 」と出力してください。
 
-       if (pass == USER_PASSWORD && name != USER_NAME){
+       else if (pass == USER_PASSWORD && name != USER_NAME){
         
         System.out.println(CONST_MSG_ERROR_NAME);
        }
@@ -65,7 +65,7 @@ public class Task1_4 {
         // ⑤ 「USER_NAME」も「USER_PASSWORD」の値も間違っていた場合。 
        //定数を使用して「 入力情報に誤りがあります。 」と出力してください。
 
-       if (name != USER_NAME && pass != USER_PASSWORD){
+       else if (name != USER_NAME && pass != USER_PASSWORD){
 
         System.out.println(CONST_MSG_ERROR_INPUT);
        }
