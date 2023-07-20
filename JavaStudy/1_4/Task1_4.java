fc3952854d40badbaa4ba36dca9b4d37fb41d3c3
YugaSeri <awcrowpile0629@outlook.jp>
2023-07-21 01:44:08 +0900

    Update Task1_4.java

--- JavaStudy/1_4/Task1_4.java
+++ JavaStudy/1_4/Task1_4.java
@@ -65,7 +65,7 @@ public class Task1_4 {
         // ⑤ 「USER_NAME」も「USER_PASSWORD」の値も間違っていた場合。 
        //定数を使用して「 入力情報に誤りがあります。 」と出力してください。
 
-       else if (name != USER_NAME && pass != USER_PASSWORD){
+       else (name != USER_NAME && pass != USER_PASSWORD){
 
         System.out.println(CONST_MSG_ERROR_INPUT);
        }
