21a4c28b864fa990826a28b893498d8276b71c52
YugaSeri <awcrowpile0629@outlook.jp>
2023-07-29 03:27:02 +0900

    check2

--- JavaStudy/Check2/src/check/Check.java
+++ JavaStudy/Check2/src/check/Check.java
@@ -1,3 +1,27 @@
-public static void main(String[] args) {
+package check;
+
+import constants.Constants;
+
+public class Check{
 	
+	public static void main(String[] args) {
+		String firstName = "芹澤";
+		String lastName = "優芽";
+		printName(firstName,lastName);
+
+		Pet pet = new Pet(Constants.CHECK_CLASS_JAVA,Constants.CHECK_CLASS_HOGE);
+			pet.introduce();
+			
+		RobotPet robotPet = new RobotPet(Constants.CHECK_CLASS_R2D2,Constants.CHECK_CLASS_LUKE);
+		robotPet.introduce();
+		}
+
+
+	private static void printName(String firstName, String lastName) {
+		System.out.println("printNameメソッド→ " + firstName + lastName);
+
+		}
+	
+	
+		
 }
